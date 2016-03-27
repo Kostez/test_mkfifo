949beea28b985e78682675f4081871cf6e560d27
Kostez <kostelom@bk.ru>
2016-03-28 02:53:39 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -57,10 +57,10 @@ int main(int argc, char** argv) {
 void runmylab(){
 	if(strcmp("", _multiplex) == 0)){
 		printf("Укажите multiplex командой: --multiplex=value\n");
-		exit 1;	
+		exit(1);	
 	}
 	if(!(strcmp(0, _multiplex) == 0)||strcmp(1, _execute ) == 0))){
 		printf("Укажите значение multiplex равное 0 или 1");
-		exit 1;
+		exit(1);
 	}
 }
