a515e1330f10f7b06f325a0aee818b18a68e49ed
Kostez <kostelom@bk.ru>
2016-03-29 16:17:41 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -99,8 +99,8 @@ void runmylab(){
 		close(pipe1[0]);
 		close(pipe1[1]);
     	
-//    	dup2(1, pipe2[1]);
-//    	dup2(0, pipe1[0]);
+    	dup2(1, pipe2[1]);
+    	dup2(0, pipe1[0]);
 
 		system(_execute);
 		printf("PARENT: конец\n");
