988517582313a029dc8303d746ec81c2fc988125
Kostez <kostelom@bk.ru>
2016-03-29 19:06:06 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -24,8 +24,7 @@ int main(int argc, char** argv) {
         switch(rez){
             case 'l': {
                 colparam = 1;
-                _logfile = ".";
-                _logfile = strcat(_logfile, optarg);
+                _logfile = strcat(".", optarg);
                 //_logfile = optarg;
                 break;
             };
