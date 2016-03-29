7d9d4508c13c0dd64f521530ed08a6d8a7f8c743
Kostez <kostelom@bk.ru>
2016-03-29 16:54:53 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -89,8 +89,8 @@ void runmylab(){
 		printf("PARENT: начало\n");
 		printf("жду:\n");
 		
-		oldstdout = dup(STDOUT);
-		oldstdin = dup(STDIN);
+		oldstdout = dup(1);
+		oldstdin = dup(0);
 		
 		while(pid = wait(&status)>0);		//wait выдает номер (pid) потомка, если не осталось потомком, то выдаст -1
     
