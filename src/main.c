bc92ff94697a46fb9204aef33eff15e5c03879b5
Kostez <kostelom@bk.ru>
2016-03-30 17:25:25 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -123,7 +123,7 @@ void runmylab(){
 			char *inexit_buf;
 			FD_ZERO(&inputs);
 			FD_SET(0, &inputs);
-			tv.tv_sec = 1;
+			tv.tv_sec = 5;
 			tv.tv_usec = 0;
 			retval = select(FD_SETSIZE, &inputs, NULL, NULL, &tv);
 			switch(retval) {
